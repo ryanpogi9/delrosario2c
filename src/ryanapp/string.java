435ce9791f6453aa4116c5218dfab7639d4b4855
ryanbang <delrosariopogisomuch@gmail.com>
2025-07-28 16:31:44 +0800

    i uploaded activity 2

--- src/ryanapp/string.java
+++ src/ryanapp/string.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
+
 package ryanapp;
 
-/**
- *
- * @author Ryan
- */
-class string {
+import java.util.Scanner;
+
+public class string {
+
+    
+    public static void main(String[] args){
+        Scanner sc =  new Scanner (System.in);
+         
+         int num;
+         System.out.print("Enter a Number: ");
+         num = sc.nextInt();
+         
+         
+         if (num == 0){
+             System.out.println("Inputed Number is Zero");
+         }else if (num < 0){
+             System.out.println(num+" is a Negative Number");
+         }
+         else
+         System.out.println(num+" is a Positive Number");
+    
+    }
     
 }
+
